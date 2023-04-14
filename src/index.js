fe8905b0387442d848ad88f26e5b97eddf846e19
linzizhan <linzizhan@corp.netease.com>
2023-04-14 16:55:49 +0800

    feat: rollup配置

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import { numberFormat, randomRange } from './num-format'
 import { getBeijingTime } from './time-format'
 import { sleep, animationSleep } from './sleep'
 
+const a = () => {
+    console.log(1111);
+}
+
 export {
     leadDebounce,
     numberFormat,
     randomRange,
     getBeijingTime,
     sleep,
-    animationSleep
+    animationSleep,
+    a
 }
 
 
